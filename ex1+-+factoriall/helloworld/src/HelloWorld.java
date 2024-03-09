afa683d306e326220214931416af34ad0a43e162
sudynoor <142943019+sudynoor@users.noreply.github.com>
2024-03-09 17:44:47 +0300

    Factorial of Number
     Factorial of Number

---
+++ ex1+-+factoriall/helloworld/src/HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+
+        public static void main(String[] args)
+        {
+
+               // Exercise 1:
+               // Write a java program that calculates the factorial of 10
+
+
+                // 10! = 10*9*8*7*6*5*4*3*2*1  = XXXXXXX
+
+                int result = 1;
+
+                for(int i=10 ; i>=1 ; i--  ){
+                        result = result * i;
+                }
+
+                System.out.print(result);
+                
+
+        }
+}
+
+
+
+
+
